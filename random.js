958733fedf80b35b7a631441fd7cb17c928f39d7
Phạm Quang Khang <khang21081995@gmail.com>
2018-09-13 10:53:34 +0700

    Create random.js

---
+++ random.js
@@ -0,0 +1,23 @@
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+}
+
+function getVlott(maxRange) {
+    let a = [];
+    let length = 6
+    maxRange = maxRange || 55;
+    while (a.length !== length) {
+        let randNum = getRandomInt(maxRange);
+        if (!a.includes(randNum)) {
+            a.push(randNum)
+        }
+    }
+    a.sort(function (a1, b) {
+        return a1 > b;
+    });
+    return {mega: '6/' + maxRange, number: a};
+}
+
+console.log(getVlott(45))
+console.log(getVlott(55))
